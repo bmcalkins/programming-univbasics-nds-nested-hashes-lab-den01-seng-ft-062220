ffc3437e081334d8df658e454732b6deb282fc20
Bradley Calkins <b.raders1013@gmail.com>
2020-06-15 18:03:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -43,7 +43,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  infinite_jest
+  infinite_jest[][] = ""
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
