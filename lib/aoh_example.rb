a63a2c73f7419e1d9d3edf6e89d6402efd72da17
Bradley Calkins <b.raders1013@gmail.com>
2020-06-15 18:09:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -50,7 +50,9 @@ def aoh_update(aoh, row, key, new_value)
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
   
-  infinite_jest[1] = {:name=>"Bob Marley", :occupation=>"Radio Personality"}
+  infinite_jest[1] = {:name=>"Bob Marley", :occupation=>"soccer player"}
+  
+  infinite_jest 
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
