aed817a8651fbeacc3dd88cd2ee73d850eff5a0b
Bradley Calkins <b.raders1013@gmail.com>
2020-06-15 18:27:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -50,7 +50,7 @@ def aoh_update(aoh, row, key, new_value)
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
   
-  infinite_jest[1][:name] = {"Joelle van Dyne (PGOAT)"}
+  infinite_jest[1][:name] = "Joelle van Dyne (PGOAT)"
   
   
   infinite_jest 
